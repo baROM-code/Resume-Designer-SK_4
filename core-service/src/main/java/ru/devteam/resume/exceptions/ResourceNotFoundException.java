bfd42dbd8100f93769786e69c0f29151cc74a863
Sergey-Kosenko <serg.cos@mail.ru>
2022-07-20 18:54:14 +0500

    Lesson_4

---
+++ core-service/src/main/java/ru/devteam/resume/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.devteam.resume.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
